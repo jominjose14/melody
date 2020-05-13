ae2b8e710352056061050200c8fd38d7e857dbab
jominjose14 <42197279+jominjose14@users.noreply.github.com>
2020-05-13 20:23:32 +0530

    Ready for deployment

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const tiles = ["one","two","three","four","five","six","seven","eight","nine","t
 const gamePattern = [];
 const playerPattern = [];
 
+var mode = 0;
+var modes = ["Easy", "Hard", "Pro"];
 var started = false;
 var level = 0;
 
@@ -28,12 +30,30 @@ $("#caption").click(() => {
 //Tile behaviour
 
 $(".piano > div").click((e) => {
-    let audio = new Audio("./audio/"+$(e.target).attr("id")+".mp3");
-    audio.play();
+    (new Audio("./audio/"+$(e.target).attr("id")+".mp3")).play();
     $(e.target).fadeOut(100).fadeIn(100);
     if(started) {
         playerPattern.push($(e.target).attr("id"));
         check(playerPattern.length-1);
+    }
+});
+
+//Heading behaviour
+$(".heading").click(() => {
+    if(!started) {
+        (new Audio("./audio/mode.mp3")).play();
+        mode = (mode + 1) % 3;
+        $(".heading").animate({"opacity":0},200,() => {
+            $(".heading").html("<span></span><span>"+modes[mode]+"</span><span> Mode</span>")
+            .animate({"opacity":1},200,() => {
+                setTimeout(() => {
+                    $(".heading").animate({"opacity":0},200,() => {
+                        $(".heading").html("<span id='m'>M</span><span id='e'>e</span><span id='l'>l</span><span id='o'>o</span><span id='d'>d</span><span id='y'>y</span>")
+                        .animate({"opacity":1},200);
+                    });
+                }, 500);
+            });
+        });
     }
 });
 
@@ -50,13 +70,13 @@ function check(index) {
     if(playerPattern[index] === gamePattern[index]) {
         if(playerPattern.length === gamePattern.length) {
             setTimeout(() => {
+                level++;
                 nextLevel();
             }, 1000);
         }
     } else {
         //audio
-        let audio = new Audio("./audio/error.mp3");
-        audio.play();
+        (new Audio("./audio/error.mp3")).play();
 
         //flash
         $("body").addClass("wrong-answer");
@@ -66,7 +86,7 @@ function check(index) {
 
         //Show score
         $("#caption").animate({"opacity":0},400,() => {
-            $("#caption").text("Score: "+(level*10)).animate({"opacity":1},400,() => {
+            $("#caption").text("Score: "+(level*100)+" in "+modes[mode]+" Mode").animate({"opacity":1},400,() => {
                 setTimeout(() => {
                     reset();
                     //Play again
@@ -80,8 +100,7 @@ function check(index) {
 }
 
 function nextLevel() {
-    //Update level & clear player's pattern
-    level++;
+    //Clear player's pattern
     playerPattern.length = 0;
 
     //Re-render Caption
@@ -90,12 +109,27 @@ function nextLevel() {
     });
 
     //Add new tile to game pattern
-    const newTile = tiles[Math.floor(Math.random()*13)];
+    let newTile = "";
+    if(mode === 2) //Pro Mode - Black & White
+        newTile = tiles[Math.floor(Math.random()*13)];
+    else if(mode === 1) { //Hard Mode - White only
+        let index = 1;
+        while(index in {1:1,3:3,6:6,8:8,10:10}) {
+            index = Math.floor(Math.random()*13);
+        }
+        newTile = tiles[index];
+    }
+    else if(mode === 0) { //Easy Mode - Black only
+        let index = 0;
+        while(index in {0:0,2:2,4:4,5:5,7:7,9:9,11:11,12:12}) {
+            index = Math.floor(Math.random()*13);
+        }
+        newTile = tiles[index];
+    }
     gamePattern.push(newTile);
 
     //Let player know new tile
-    let audio = new Audio("./audio/"+newTile+".mp3");
-    audio.play();
+    (new Audio("./audio/"+newTile+".mp3")).play();
     $("#"+newTile).addClass("new-tile");
     setTimeout(() => {
         $("#"+newTile).removeClass("new-tile");
--- openingAnimation.js
+++ openingAnimation.js
@@ -1,14 +1,16 @@
-let audio = new Audio("./audio/intro.mp3");
-audio.play();
+(new Audio("./audio/intro.mp3")).play();
 
-$(".heading > span").css("opacity",0);
+$(".heading").removeClass("hide");
+$(".heading").css("opacity",0).animate({"opacity":1},1500);
 
-$("#m").animate({"opacity":1},300,() => {
-    $("#e").animate({"opacity":1},300,() => {
-        $("#l").animate({"opacity":1},300,() => {
-            $("#o").animate({"opacity":1},300,() => {
-                $("#d").animate({"opacity":1},300,() => {
-                    $("#y").animate({"opacity":1},300);
+$("#seven").css("opacity",0).animate({"opacity":1},200,() => {
+    $(".piano > :nth-child(6), .piano > :nth-child(8)").css("opacity",0).animate({"opacity":1},200,() => {
+        $(".piano > :nth-child(5), .piano > :nth-child(9)").css("opacity",0).animate({"opacity":1},200,() => {
+            $(".piano > :nth-child(4), .piano > :nth-child(10)").css("opacity",0).animate({"opacity":1},200,() => {
+                $(".piano > :nth-child(3), .piano > :nth-child(11)").css("opacity",0).animate({"opacity":1},200,() => {
+                    $(".piano > :nth-child(2), .piano > :nth-child(12)").css("opacity",0).animate({"opacity":1},200,() => {
+                        $(".piano > :nth-child(1), .piano > :nth-child(13)").css("opacity",0).animate({"opacity":1},200);
+                    });
                 });
             });
         });
@@ -22,7 +24,7 @@ setTimeout(() => {
     $("#o").css("color","#b30909");
     $("#d").css("color","#fff");
     $("#y").css("color","#b30909");
-},2500);
+},1500);
     
 setTimeout(() => {
     $("#m").css("color","#b30909");
@@ -31,10 +33,10 @@ setTimeout(() => {
     $("#o").css("color","#fff");
     $("#d").css("color","#b30909");
     $("#y").css("color","#fff");
-},2700);
+},1600);
 
 setTimeout(() => {
     $("#caption").animate({"opacity":0},400,() => {
         $("#caption").text("Click here to start").animate({"opacity":1},400);
     });
-}, 3000);
+}, 2700);
